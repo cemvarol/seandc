1e2abec64e9e210106ac4323cb617824b46c0213
cemvarol <cem.varol@outlook.com>
2020-12-02 17:24:34 +0000

    Update 1-SetDCReady.ps1

--- 1-SetDCReady.ps1
+++ 1-SetDCReady.ps1
@@ -1,21 +1,25 @@
 Set-NetFirewallProfile -Enabled False
 
 
-$url = "https://raw.githubusercontent.com/cemvarol/DC/master/ChromeInstall.ps1"
+$url1 = "https://raw.githubusercontent.com/cemvarol/DC/master/ChromeInstall.ps1"
 $output1 = "$env:USERPROFILE\downloads\ChromeInstall.ps1"
-Invoke-WebRequest -Uri $url -OutFile $output1
+Invoke-WebRequest -Uri $url1 -OutFile $output1
 
-$url = "https://raw.githubusercontent.com/cemvarol/seandc/main/OneTouchDCInstallation.ps1"
+$url2 = "https://raw.githubusercontent.com/cemvarol/seandc/main/OneTouchDCInstallation.ps1"
 $output2 = "$env:USERPROFILE\desktop\InstallDC.ps1"
-Invoke-WebRequest -Uri $url -OutFile $output2
+Invoke-WebRequest -Uri $url2 -OutFile $output2
 
-$url = "https://raw.githubusercontent.com/cemvarol/seandc/main/50-ADUsers.ps1"
+$url3 = "https://raw.githubusercontent.com/cemvarol/seandc/main/50-ADUsers.ps1"
 $output3 = "$env:USERPROFILE\desktop\50-ADusers.ps1"
-Invoke-WebRequest -Uri $url -OutFile $output3
+Invoke-WebRequest -Uri $url3 -OutFile $output3
 
-$url = "https://raw.githubusercontent.com/cemvarol/seandc/main/Office2016.url"
+$url4 = "https://raw.githubusercontent.com/cemvarol/seandc/main/Office2016.url"
 $output4 = "$env:USERPROFILE\desktop\Office2016.url"
-Invoke-WebRequest -Uri $url -OutFile $output4
+Invoke-WebRequest -Uri $url4 -OutFile $output4
+
+$url5 = "https://github.com/cemvarol/DC/raw/master/Active%20Directory%20Users%20and%20Computers.lnk"
+$output5 = "$env:USERPROFILE\desktop\Active Directory Users and Computers.lnk"
+Invoke-WebRequest -Uri $url5 -OutFile $output5
 
 Start-Sleep -s 3
 
